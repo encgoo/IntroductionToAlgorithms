edc021b34dce7dda6847d176ba0f1d673293e5ab
Yongjian Feng <yongjianfeng@Yongjians-MBP.fios-router.home>
2022-02-26 23:06:00 -0500

    InsertSort

---
+++ Sorting/InsertSort.py
@@ -0,0 +1,38 @@
+
+def print_lst(lst):
+    print(' '.join(map(lambda x:str(x), lst)))
+
+
+def insert_last(lst, e):
+    i_val = lst[e]
+
+    ind = e - 1
+    while ind >=0 and lst[ind] > i_val:
+        lst[ind + 1] = lst[ind]
+        #print_lst(lst)
+        ind -= 1
+
+    lst[ind + 1] = i_val
+    print_lst(lst)
+
+
+def insert_sort(lst):
+    n = len(lst)
+    # idx = n - 1
+    # val = lst[idx]
+    # while idx > 0:
+    #     insert_last(lst, idx)
+    #     if val == lst[idx]:
+    #         idx -= 1
+    #     val = lst[idx]
+    idx = 1
+    while idx < n:
+        insert_last(lst, idx)
+        idx += 1
+
+
+if __name__ == '__main__':
+    lst = [3,2,1,5,6,9, 8]
+    #insert_last(lst, len(lst) - 1)
+    insert_sort(lst)
+    print(lst)
