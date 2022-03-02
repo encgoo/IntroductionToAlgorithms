f7ebb203ef1d72be3fb370c5fe899490534724cc
Yongjian Feng <yongjianfeng@Yongjians-MBP.fios-router.home>
2022-03-01 21:45:11 -0500

    Clean up

--- AdvDesign/GreedyAlgorithm/Candies.py
+++ AdvDesign/GreedyAlgorithm/Candies.py
@@ -34,39 +34,6 @@ def count_candies(scrs):
     return sum(candis)
 
 
-def candies(n, arr):
-    if n == 0:
-        return 0
-
-    candy_kid = [0] * n
-    # at least one candy for each kid
-    num_candies_to_give = 1
-    candy_kid[0] = num_candies_to_give
-
-    for index in range(1, len(arr)):
-        if arr[index] > arr[index - 1]:
-            # need to give more candy
-            num_candies_to_give += 1
-        else:
-            num_candies_to_give = 1
-        candy_kid[index] = num_candies_to_give
-
-    # reset
-    num_candies_to_give = 1
-    # init
-    total_candies = candy_kid[-1]
-    for index in range(len(arr) - 2, -1, -1):
-        if arr[index] > arr[index + 1]:
-            num_candies_to_give += 1
-        else:
-            num_candies_to_give = 1
-
-        total_candies += max(candy_kid[index], num_candies_to_give)
-
-    return total_candies
-
 if __name__ == '__main__':
-    #scrs = [2,4,2,6,1,9,8,7,2,1]
-    scrs = [51,36,36,53,25,80]
+    scrs = [2,4,2,6,1,9,8,7,2,1]
     print(count_candies(scrs))
-    print(candies(len(scrs), scrs))
